995bf044939196a4861b1fce558a3f14c58c29d1
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-30 16:20:09 +0000

    created clear all and add 1 buttons

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -7,6 +7,7 @@ interface TaskType {
 
 export default function MainContent(): JSX.Element {
   const [tasks, setTasks] = useState<TaskType[] | []>([]);
+  const [btnPressed, setBtnPressed] = useState<boolean>(false);
   useEffect(() => {
     const fetchAllTasks = async () => {
       const axiosResponse = await axios.get(
@@ -16,14 +17,32 @@ export default function MainContent(): JSX.Element {
       console.log("The status code of response is", axiosResponse.status);
     };
     fetchAllTasks();
-  }, []);
+  }, [btnPressed]);
   console.log(tasks);
   return (
     <>
       <h1>ALL TASKS</h1>
       {tasks.map((task, i) => (
-        <p key={i}>{task}</p>
+        <p key={i}>{task.id}</p>
       ))}
+      <button
+        onClick={async () => {
+          await axios.post("https://fullstack-todo.onrender.com/tasks", {
+            id: 1,
+          });
+          setBtnPressed((prev) => !prev);
+        }}
+      >
+        add 1
+      </button>
+      <button
+        onClick={async () => {
+          await axios.patch("https://fullstack-todo.onrender.com/tasks/reset");
+          setBtnPressed((prev) => !prev);
+        }}
+      >
+        Clear All
+      </button>
     </>
   );
 }
