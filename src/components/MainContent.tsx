73a87ccc552b275ac03a4d337c983aca640c7678
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-01 13:43:12 +0000

    added task button

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,11 +3,13 @@ import axios from "axios";
 
 interface TaskType {
   id: number;
+  task: string;
 }
 
 export default function MainContent(): JSX.Element {
   const [tasks, setTasks] = useState<TaskType[] | []>([]);
   const [btnPressed, setBtnPressed] = useState<boolean>(false);
+  const [inputVal, setInputVal] = useState<string>("");
   useEffect(() => {
     const fetchAllTasks = async () => {
       const axiosResponse = await axios.get(
@@ -22,18 +24,17 @@ export default function MainContent(): JSX.Element {
   return (
     <>
       <h1>ALL TASKS</h1>
-      {tasks.map((task, i) => (
-        <p key={i}>{task.id}</p>
-      ))}
       <button
         onClick={async () => {
           await axios.post("https://fullstack-todo.onrender.com/tasks", {
-            id: 1,
+            id: tasks.length + 1,
+            task: inputVal,
           });
           setBtnPressed((prev) => !prev);
+          setInputVal("");
         }}
       >
-        add 1
+        add task
       </button>
       <button
         onClick={async () => {
@@ -43,6 +44,16 @@ export default function MainContent(): JSX.Element {
       >
         Clear All
       </button>
+      <p>Add a task name below!</p>
+      <input
+        value={inputVal}
+        onChange={(e) => setInputVal(e.target.value)}
+      ></input>
+      {tasks.map((task, i) => (
+        <p key={i}>
+          Task Number {task.id}: {task.task}
+        </p>
+      ))}
     </>
   );
 }
