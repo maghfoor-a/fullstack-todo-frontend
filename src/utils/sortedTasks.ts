e3ec066f802bc99cf140dd1317d1a045fdd99d66
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-17 18:05:34 +0000

    removed task function files

--- src/utils/sortedTasks.ts
+++
@@ -1,8 +0,0 @@
-interface TaskType {
-  id: number;
-  status: boolean;
-  task: string;
-}
-export function sortedTasks(tasks: TaskType[]): TaskType[] {
-  return tasks.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
-}
